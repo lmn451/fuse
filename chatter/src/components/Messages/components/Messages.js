7437f594cabc2f035d4b49dd28da815557cc17f0
lmn451 <cia_agent1@mail.ru>
2022-03-21 12:47:39 +0200

    added scroll to last message

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
 import useSound from "use-sound";
 import config from "../../../config";
@@ -10,53 +10,66 @@ import Message from "./Message";
 import "../styles/_messages.scss";
 import { useActiveChatContext } from "../../../contexts/ActiveChat";
 
-const socket = io(config.BOT_SERVER_ENDPOINT, {
-  transports: ["websocket", "polling", "flashsocket"],
-});
+const connectToChatServer = () => {
+  const socket = io(config.BOT_SERVER_ENDPOINT, {
+    transports: ["websocket", "polling", "flashsocket"],
+  });
+  socket.onAny((type, message) => console.log({ type, message }));
+  return socket;
+};
 
 function Messages() {
   const [playSend] = useSound(config.SEND_AUDIO_URL);
   const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
-  const { setLatestMessage, messages } = useMessagesContext();
+  const [messages, setMessages] = useState([]);
+  const { setLatestMessage } = useMessagesContext();
   const { activeChatId } = useActiveChatContext();
   const [botTyping, setBotTyping] = useState(false);
   const [message, setMessage] = useState("");
   const onChangeMessage = (e) => setMessage(e.target.value);
+  const socketRef = useRef(null);
+  const listRef = useRef(null);
+
   const sendMessage = () => {
     playSend();
-    console.log("hello");
+    socketRef.current.emit("user-message", message);
   };
-
   useEffect(() => {
+    const socket = connectToChatServer();
+    socketRef.current = socket;
     const eventName = activeChatId === "bot" ? "bot-message" : "user-message";
+    const scrollToLastMessage = () => {
+      const lastChild = listRef.current.lastElementChild;
+      lastChild.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      });
+    };
     socket.on(eventName, (data) => {
       playReceive();
+      scrollToLastMessage();
       setLatestMessage(data.userId, data.message);
+      setMessages((prev) => [...prev, data.message]);
     });
-    return () => {
-      socket.off("message");
-    };
-  }, [activeChatId, playReceive, setLatestMessage]);
-
-  useEffect(() => {
     socket.on("bot-typing", () => {
       setBotTyping(true);
     });
     return () => {
+      socket.off("message");
       socket.off("bot-typing");
-      setBotTyping(false);
     };
-  });
+  }, [activeChatId, playReceive, setLatestMessage]);
 
   return (
     <div className="messages">
       <Header />
       <div className="messages__list" id="message-list">
-        {/* <li>
-          {messages.map((message) => (
-            <ol>{message}</ol>
+        <li ref={listRef}>
+          {messages.map((message, idx) => (
+            <ul key={idx}>{message}</ul>
           ))}
-        </li> */}
+        </li>
       </div>
       {botTyping && <TypingMessage />}
       <Footer
